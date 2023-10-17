afafc6a706fcf893a8f54a0e05296a98525b2e28
yanik1337 <dlagdakkaunt@gmail.com>
2023-10-17 20:07:01 +0300

    commit

---
+++ MyShoppingList1.py
@@ -0,0 +1,13 @@
+shoppinglist = []
+shoppinglist.append("Apple")
+shoppinglist.append("Milk")
+shoppinglist.append("Bread")
+shoppinglist.append("Eggs")
+shoppinglist.append("Juice")
+print(shoppinglist)
+shoppinglist.pop(1)
+print(shoppinglist)
+shoppinglist[0] = "Banana"
+print(shoppinglist)
+mynumberofpurchases = len(shoppinglist)
+print("Количество покупок:", mynumberofpurchases)
